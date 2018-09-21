4774aef51c3f18f68415df4bcae807d40a1a53ef
Adam Kepecs <adam.kepecs@gmail.com>
2018-09-21 10:35:20 -0400

    Replace token creator with V2 in tsipi Stripe.

--- src/components/stripeAddCard/index.js
+++ src/components/stripeAddCard/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { getCardToken } from '../../common/stripe'
+import stripe from 'tipsi-stripe'
 
 import AddCard from '../addCard'
 
@@ -16,7 +16,26 @@ export default class StripeAddCard extends Component {
         {...this.props}
         addCardHandler={(cardNumber, expiry, cvc) => {
           const [expiryMonth, expiryYear] = expiry.split('/')
-          return getCardToken(cardNumber, expiryMonth, expiryYear, cvc, this.props.publicStripeKey).then(token => this.props.addCardTokenHandler(token))
+          const params = {
+            // mandatory
+            number: '4242424242424242',
+            expMonth: 11,
+            expYear: 17,
+            cvc: '223',
+            // optional
+            name: 'Test User',
+            currency: 'usd',
+            addressLine1: '123 Test Street',
+            addressLine2: 'Apt. 5',
+            addressCity: 'Test City',
+            addressState: 'Test State',
+            addressCountry: 'Test Country',
+            addressZip: '55555',
+          }
+          console.log(params);
+          console.log(Stripe.createTokenWithCard(params));
+          return Stripe.createTokenWithCard(params);
+          //return getCardToken(cardNumber, expiryMonth, expiryYear, cvc, this.props.publicStripeKey).then(token => this.props.addCardTokenHandler(token))
         }}
       />
     )
